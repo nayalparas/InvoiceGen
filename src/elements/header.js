8d3fa0b4bebb6852cac7da8b37fee98d7d8dc4b5
nayalparas <nayalparas@gmail.com>
2024-02-07 18:33:24 +0530

    add text above logo

--- src/elements/header.js
+++ src/elements/header.js
@@ -4,6 +4,10 @@ module.exports = (doc) => {
 
   doc.addImage(indocare, 'JPG', 5, 10, 68, 25, '', 'SLOW');
 
+  doc.setFont(undefined, 'bold');
+  doc.setFontSize(9);
+  doc.text("Preventive Health Checkup", 29, 15.5);
+
   // doc.setFillColor(243,94,11);
   // doc.rect(10, 10, 190, 2, 'F');
 
